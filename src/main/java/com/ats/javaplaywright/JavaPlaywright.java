13abfc74049c555be04eddfd0adf6535180d3529
chaitanyamehta2018 <chaitanyamehta2018@gmail.com>
2025-03-08 18:44:17 +0530

    First script for Playwright created.

---
+++ src/main/java/com/ats/javaplaywright/JavaPlaywright.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ats.javaplaywright;
+
+import com.microsoft.playwright.*;
+
+/**
+ *
+ * @author HerryPotter
+ */
+public class JavaPlaywright {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
